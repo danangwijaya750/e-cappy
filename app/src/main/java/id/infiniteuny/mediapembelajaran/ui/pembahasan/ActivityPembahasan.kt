bef0c428858dbc7d3f88d4bc11017660b614345b
Danang <danangwijaya750@gmail.com>
2020-01-20 17:42:32 +0700

    early pembahasan

--- app/src/main/java/id/infiniteuny/mediapembelajaran/ui/pembahasan/ActivityPembahasan.kt
+++ app/src/main/java/id/infiniteuny/mediapembelajaran/ui/pembahasan/ActivityPembahasan.kt
@@ -1,13 +1,42 @@
 package id.infiniteuny.mediapembelajaran.ui.pembahasan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager.LayoutParams
+import androidx.appcompat.app.AppCompatActivity
 import id.infiniteuny.mediapembelajaran.R
+import kotlinx.android.synthetic.main.activity_detail_materi.web_view
+import kotlinx.android.synthetic.main.activity_quiz_result.tool_bar
 
 class ActivityPembahasan : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_ACTION_BAR)
         setContentView(R.layout.activity_pembahasan)
+        if (VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+            val w = window
+            w.setFlags(
+                LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+        }
+        supportActionBar?.hide()
+        loadData()
+    }
+
+    private fun loadData() {
+        when (intent.getStringExtra("caller")) {
+            "eval" -> {
+                tool_bar.setBackgroundColor(resources.getColor(R.color.lightBlue))
+                web_view.loadUrl("file:///android_res/raw/pmbhs_eval")
+            }
+            else -> {
+                tool_bar.setBackgroundColor(resources.getColor(R.color.softYellow))
+                web_view.loadUrl("file:///android_res/raw/pmbhs_train")
+            }
+        }
     }
 }
--- app/src/main/java/id/infiniteuny/mediapembelajaran/ui/rekap/TableAdapter.kt
+++
@@ -1,7 +0,0 @@
-package id.infiniteuny.mediapembelajaran.ui.rekap
-
-import com.evrencoskun.tableview.adapter.AbstractTableAdapter
-
-/**
- * Created by wijaya on 14/01/20
- */
