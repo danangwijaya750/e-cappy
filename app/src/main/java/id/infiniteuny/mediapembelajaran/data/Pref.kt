09f09cc56a9e9757816f76a8fb703c4c55d13df3
Danang <danangwijaya750@gmail.com>
2020-01-29 22:10:03 +0700

    early sound button

--- app/src/main/java/id/infiniteuny/mediapembelajaran/data/Pref.kt
+++ app/src/main/java/id/infiniteuny/mediapembelajaran/data/Pref.kt
@@ -9,9 +9,14 @@ import android.content.SharedPreferences
 class Pref (context: Context) {
     val PREFS_FILENAME = "id.infiniteuny.mediapembelajaran"
     val NAME = "name"
+    val SOUND="sound"
     val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
 
     var user_name: String
         get() = prefs.getString(NAME,"halo").toString()
         set(value) = prefs.edit().putString(NAME,value).apply()
+
+    var sound_setting:Boolean
+    get()=prefs.getBoolean(SOUND,true)
+    set(value) = prefs.edit().putBoolean(SOUND,value).apply()
 }
